1717bec8d9f25a47037320354262b6c3dba4f68b
markdmckenna <markdelaneymckenna@gmail.com>
2020-02-16 22:19:00 +0100

    SetterInjectedController

---
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.sevices.GreetingService;
+
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.getGreeting();
+    }
+
+}
